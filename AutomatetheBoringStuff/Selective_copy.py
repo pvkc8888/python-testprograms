b8bcedc733547730674b11cce8d7aa5da6c0ac37
Vinay Pulivarthi <pvkc8888@yahoo.com>
2017-12-09 16:37:10 -0500

    select images no more permission denied error

--- AutomatetheBoringStuff/Selective_copy.py
+++ AutomatetheBoringStuff/Selective_copy.py
@@ -1,11 +1,13 @@
 import re, os, shutil
 
-ReqfileName = re.compile(r'.*\.jpg')
+ReqfileName = re.compile(r'.*\.jpg$')
 
 CurrentDir = "C:\\Users\\pvkc"
 for folders, Sub, files in os.walk(CurrentDir):
         for items in files:
                 if ReqfileName.match(items):
-                        print(folders,items)
-                        #shutil.copy(folders,'C:\\Users\\pvkc\\Desktop\\testfolder')
-				
+                        try:
+                                shutil.copy(os.path.abspath(folders)+'\\'+items,'C:\\Users\\pvkc\\Desktop\\testfolder')
+                        except:
+                                print(folders,files)
+                                print('File exists already!')
