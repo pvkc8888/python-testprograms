bf61364d841f079168e53bcb55de325b675b398f
Vinay Kumar Chowdary Pulivarthi <pvkc@umd.edu>
2017-12-11 10:14:39 -0500

    testing out bs4 with college website

--- Sample tests/testing_bs4.py
+++ Sample tests/testing_bs4.py
@@ -1,11 +1,10 @@
 import requests, sys, webbrowser, bs4
 res = requests.get('http://cee.umd.edu/faculty')
 res.raise_for_status()
-soup = bs4.BeautifulSoup(res.text)
+soup = bs4.BeautifulSoup(res.text,"lxml")
 
-linkElems = soup.find_all("div",{"class":"view view-faculty view-id-faculty view-display-id-page view-dom-id-8924432f221617777408fb3bc0dc79da"})
-for items in linkElems:
+for faculty in soup.find_all("div",{"class":"view-content"}):
 	try:
-		print(items.contents[1].text)
+		print(faculty.div.span.a.text)
 	except:
-		pass	
+		pass
