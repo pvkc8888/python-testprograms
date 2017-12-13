e9ce297aa342d4fdf2e7bbfdbf7b7d84dbbc405f
Vinay Kumar Chowdary Pulivarthi <pvkc@umd.edu>
2017-12-13 16:08:09 -0500

    trying easy 343

---
+++ Daily Programmer/Easy#344.py
@@ -0,0 +1,17 @@
+def convert2binary(n):
+	converted_number = []
+	while(n!=0):
+		if n%2 == 0:
+			converted_number.append(0)
+			print(converted_number)
+			n = n/2
+			print(n)
+		elif n%2 == 1:
+			converted_number.append(0)
+n = n/2
+			print(converted_number)
+			print(n)
+		print(converted_number)
+		return converted_number
+example = convert2binary(100)
+print(example)
