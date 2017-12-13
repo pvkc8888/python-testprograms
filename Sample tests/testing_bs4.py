eeb8b780171ec88b3e3ab61e5d188775b301af48
Vinay Pulivarthi <pvkc8888@yahoo.com>
2017-12-12 23:14:33 -0500

    Working to find all names, ph no and stuff individually

--- Sample tests/testing_bs4.py
+++ Sample tests/testing_bs4.py
@@ -2,9 +2,16 @@ import requests, sys, webbrowser, bs4
 res = requests.get('http://cee.umd.edu/faculty')
 res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text,"lxml")
-
-for faculty in soup.find_all("div",{"class":"view-content"}):
+faculty = soup.find_all("div",{"id":"quicktabs-tabpage-faculty-0"})[0]
+#print(faculty.find_all("div",{"class":"view-content"}))
+for item in faculty.find_all("div",{"class":"views-row"}):
+	#print(item)
 	try:
-		print(faculty.div.span.a.text)
-	except:
-		pass
+		print(item.find_all("span")[0].text)
+		print(item.find_all("div",{"class":"views-field views-field-field-title"})[0].text)
+		print(item.find_all("div",{"class":"views-field views-field-field-address"})[0].text)
+		print(item.find_all("span",{"class":"views-field views-field-field-phone"})[0].text)
+		print(item.find_all("span",{"class":"views-field views-field-field-email"})[0].text)
+	except Exception as e:
+		print(e)
+	print()
