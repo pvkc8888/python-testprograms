37f6b89a71f8081740e4afc2a732b536950d1439
Vinay Kumar Chowdary Pulivarthi <pvkc@umd.edu>
2017-12-13 17:08:56 -0500

    completed Solution for Easy#344

--- Daily Programmer/Easy#344.py
+++ Daily Programmer/Easy#344.py
@@ -1,17 +1,34 @@
+import re
+
+#This function converts decimal number to binary and send as a list
 def convert2binary(n):
-	converted_number = []
-	while(n!=0):
+	binary_number = []
+	while True:
 		if n%2 == 0:
-			converted_number.append(0)
-			print(converted_number)
-			n = n/2
-			print(n)
+			binary_number = [0] + binary_number			
+			n = int(n/2)			
 		elif n%2 == 1:
-			converted_number.append(0)
-n = n/2
-			print(converted_number)
-			print(n)
-		print(converted_number)
-		return converted_number
-example = convert2binary(100)
-print(example)
+			binary_number = [1] + binary_number
+			n = int(n/2)			
+		if n == 0:
+			#binary_number = [0] + binary_number
+			break
+	return binary_number
+
+#regular expression for checking 0's
+total = re.compile('0*')
+numbers = input('Enter the number of numbers')
+print("1",end = "")
+for item in range(1,int(numbers)+1):
+	binary = convert2binary(item)
+	binary = list(map(str,binary))	
+	binary = "".join(binary)
+	find = total.findall(binary)
+	find = list(map(len,find))
+	for item in find:
+		if item%2 != 0:
+			number = 0
+			break
+		else:
+			number = 1
+	print(", ",number,end="")
