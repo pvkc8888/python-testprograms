07453c88ed104ad3e22e4afeaa828a7555ffa7de
Vinay Pulivarthi <pvkc8888@yahoo.com>
2017-12-13 00:21:39 -0500

    Yaay! writing CSV files also done!

--- Sample tests/testing_bs4.py
+++ Sample tests/testing_bs4.py
@@ -6,7 +6,7 @@ res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text,"lxml")
 filename = 'Civil_Contacts.csv'
 with open('Civil_Contacts.csv', 'w') as new_file:
-	fieldnames = ['Name','Address','Contact number','email id']
+	fieldnames = ['Name','Field title','Address','Contact number','email id']
 	writer = csv.DictWriter(new_file, delimiter = ",",fieldnames = fieldnames)
 	writer.writeheader()
 	faculty = soup.find_all("div",{"id":"quicktabs-tabpage-faculty-0"})[0]
@@ -14,29 +14,30 @@ with open('Civil_Contacts.csv', 'w') as new_file:
 	for item in faculty.find_all("div",{"class":"views-row"}):
 		try:
 			print(item.find_all("span")[0].text)
-			writer.writerow(item.find_all("span")[0].text)
+			name = item.find_all("span")[0].text
 
 		except:
-			writer.writerow('Name',item.find_all("span")[0].text)
+			name = "No name given"
 		try:	
 			print(item.find_all("div",{"class":"views-field views-field-field-title"})[0].text)
-			writer.writerow(item.find_all("div",{"class":"views-field views-field-field-title"})[0].text)
+			field = item.find_all("div",{"class":"views-field views-field-field-title"})[0].text
 		except:
-			writer.writerow(item.find_all("span")[0].text)
+			field = "None"
 		try:	
 			print(item.find_all("div",{"class":"views-field views-field-field-address"})[0].text)
-			writer.writerow(item.find_all("div",{"class":"views-field views-field-field-address"})[0].text)
+			address = item.find_all("div",{"class":"views-field views-field-field-address"})[0].text
 		except:
-			writer.writerow(item.find_all("span")[0].text)
+			address = "None"
 		try:
 			print(item.find_all("span",{"class":"views-field views-field-field-phone"})[0].text)
-			writer.writerow(item.find_all("span",{"class":"views-field views-field-field-phone"})[0].text)
+			cno = item.find_all("span",{"class":"views-field views-field-field-phone"})[0].text
 		except:
-			writer.writerow(item.find_all("span")[0].text)
+			cno = "None"
 		try:
 			print(item.find_all("span",{"class":"views-field views-field-field-email"})[0].text)
-			writer.writerow(item.find_all("span",{"class":"views-field views-field-field-email"})[0].text)
+			email = item.find_all("span",{"class":"views-field views-field-field-email"})[0].text
 		except:
-			writer.writerow(item.find_all("span")[0].text)
+			email = "None"
+		writer.writerow({"Name":name,"Field title":field,"Address":address,"Contact number":cno,"email id":email})
 		print()
-writer.close()
+#writer.close()
