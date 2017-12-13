105d6743b0a7135bac503dba49065119fddf3022
Vinay Pulivarthi <pvkc8888@yahoo.com>
2017-12-12 23:59:20 -0500

    testing to write files

--- Sample tests/testing_bs4.py
+++ Sample tests/testing_bs4.py
@@ -5,9 +5,9 @@ res.raise_for_status()
 
 soup = bs4.BeautifulSoup(res.text,"lxml")
 filename = 'Civil_Contacts.csv'
-with open('Civil_Contacts.csv', 'wb') as new_file:
+with open('Civil_Contacts.csv', 'w') as new_file:
 	fieldnames = ['Name','Address','Contact number','email id']
-	writer = csv.DictWriter(new_file, fieldnames = fieldnames, delimiter = "\t")
+	writer = csv.DictWriter(new_file, delimiter = ",",fieldnames = fieldnames)
 	writer.writeheader()
 	faculty = soup.find_all("div",{"id":"quicktabs-tabpage-faculty-0"})[0]
 
@@ -17,7 +17,7 @@ with open('Civil_Contacts.csv', 'wb') as new_file:
 			writer.writerow(item.find_all("span")[0].text)
 
 		except:
-			writer.writerow(item.find_all("span")[0].text)
+			writer.writerow('Name',item.find_all("span")[0].text)
 		try:	
 			print(item.find_all("div",{"class":"views-field views-field-field-title"})[0].text)
 			writer.writerow(item.find_all("div",{"class":"views-field views-field-field-title"})[0].text)
