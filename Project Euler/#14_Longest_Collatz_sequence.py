d8df9b0d3286a533d0a750588171ac3d770c3b4c
Vinay Kumar Chowdary Pulivarthi <pvkc@umd.edu>
2018-01-02 14:43:16 -0500

    Soltuion to problem 14 done!

--- Project Euler/#14_Longest_Collatz_sequence.py
+++ Project Euler/#14_Longest_Collatz_sequence.py
@@ -1,51 +1,51 @@
 import sys
-
-listt = [0]*5000000
-highest = [0]*5000000
+listt = [0]*3732450 #change the size according to your  needs.
+#I am using 3732450 because after that the 
+highest = [0]*3732450
 listt[0] = 0
 listt[1] = 0
 listt[2] = 1
 
 def collatz(n):
     temp = n
-    counter = 1
+    counter = 0
     while n>1:
         if n%2==0:
-            n = int(n/2)
-            try:
-                if listt[n] != 0:
-                    counter += listt[n]
-                    break
-                else:
-                    counter = counter+1
-            except:
-                pass
+            n = n//2
+            counter = counter+1
+            if n<3732449 and listt[n] != 0:
+                counter += listt[n]
+                listt[temp]=counter
+                break
         else:
             n = 3*n+1
-            try:
-                if listt[n] != 0:
-                    counter += listt[n]
-                    break
-                else:
-                    counter = counter+1
-            except:
-                pass
-    listt[temp]=counter       
-for i in range(3,3000000,1):
+            counter = counter+1
+            if n<3732449 and listt[n] != 0:
+                counter += listt[n]
+                listt[temp]=counter
+                break
+           
+for i in range(3,3732430,1):
     collatz(i)
     
 maxx = 0
-for i in range(3000000):
+for i in range(3732430):
     if listt[i]>=maxx:
         highest[i] = i
         maxx = listt[i]
     else:
         highest[i] = highest[i-1]
-        
+     
 t = int(input().strip())
 for a0 in range(t):
     n = int(input().strip())
-    if(n>2999999):
-        print(2952846)
-        break
+    if n>=5649499 #this is actually not needed.
+        print(5649499)
+        continue  #continue the loop, dont break it like i did.
+        
+    if n>=3732423: #calculated already
+        print(3732423)
+        continue
+    
     print(highest[n])
+    #after 6hours finally!
