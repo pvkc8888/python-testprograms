99107ee447989375c1111b09d88e33d4b575ac13
Vinay Pulivarthi <pvkc8888@yahoo.com>
2018-01-23 16:54:43 -0500

    one case wrong?

---
+++ Algos Hackerrank/Warmup/Time_Conversion.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import sys
+
+def timeConversion(s):
+    if s[8]=='A':
+        if s[0]=='1' and s[1]=='2':
+            print('00'+s[2:8])
+        else:
+            print(s[:8])
+    else:
+        temp = int(s[0:2])
+        print(str(12+temp)+s[2:8])
+s = input().strip()
+result = timeConversion(s)
