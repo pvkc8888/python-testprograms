9a4fdaff08786150c94dbc8831002d6f65de9d4c
Vinay Pulivarthi <pvkc8888@yahoo.com>
2018-01-23 16:30:20 -0500

    compared to cpp its easy in python

---
+++ Algos Hackerrank/Warmup/Staircase.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+import sys
+
+def staircase(n):
+    for i in range(1,n+1):
+        print(' '*(n-i),'#'*(i),sep='')
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    staircase(n)
