27af177bd356af3014e29b4f2a65677d1a7d6e12
Vinay Pulivarthi <pvkc8888@yahoo.com>
2018-01-25 19:09:31 -0500

    documentation

---
+++ Algos Hackerrank/Implementation/Day_of_the_Programmer.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import sys
+
+def solve(year):
+    if year >= 1919:
+        if year%400 == 0:
+            return('12.09.'+str(year))
+        elif year%4 == 0 and year % 100 != 0:
+            return('12.09.'+str(year))
+        else:
+            return('13.09.'+str(year))
+    if year == 1918:
+        return('26.09.1918')
+    else:
+        if year%4 == 0:
+            return('12.09.'+str(year))
+        else:
+            return('13.09.'+str(year))
+
+year = int(input().strip())
+result = solve(year)
+print(result)
