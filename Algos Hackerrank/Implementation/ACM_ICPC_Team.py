30362254b44fcb4398e7adbe3c1042e6af9f0db4
Vinay Pulivarthi <pvkc8888@yahoo.com>
2018-02-16 15:24:13 -0500

    Good problem

--- Algos Hackerrank/Implementation/ACM_ICPC_Team.py
+++ Algos Hackerrank/Implementation/ACM_ICPC_Team.py
@@ -2,21 +2,22 @@
 
 import sys,itertools
 
-def acmTeam(topic):
-    participants = len(topic)
-    listt = [0]*(2*len(topic[0])+1)
-    for item in range(participants):
-        for item2 in range(item+1,participants):
-            count = 0
-            teamscore = int(topic[item])+int(topic[item2])
-            for values in str(teamscore):
-                if int(values) >= 1:
-                    count += 1
-            listt[count] += 1
-    for i in range(len(listt)-1,0,-1):
-        if listt[i] != 0:
-            return i,listt[i]
-        
+def acmTeam(topic,n):
+    Dict = {}
+    teams = itertools.combinations(range(n),2)
+    for item in teams:
+        count = 0
+        teamtotal = int(topic[item[0]]) + int(topic[item[1]])
+        teamtotal = str(teamtotal)
+        for value in range(len(teamtotal)):
+            if teamtotal[value] == '1' or teamtotal[value] == '2' :
+                count += 1
+        #print(teamtotal,count)
+        if count not in Dict:
+            Dict[count] = 1
+        else:
+            Dict[count] += 1
+    return [max(Dict),Dict[max(Dict)]]
 if __name__ == "__main__":
     n, m = input().strip().split(' ')
     n, m = [int(n), int(m)]
@@ -25,7 +26,5 @@ if __name__ == "__main__":
     for topic_i in range(n):
        topic_t = str(input().strip())
        topic.append(topic_t)
-    result = acmTeam(topic)
+    result = acmTeam(topic,n)
     print ("\n".join(map(str, result)))
-
-
